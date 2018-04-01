78cbf66b3ede713bd953c1b5b9792a0aa5a446ef
Vijay V <vijay.v@aurea.com>
2018-04-01 22:27:30 +0530

    Moving to gradle from maven

--- src/main/java/api/kcorp/message/Receiver.java
+++ src/main/java/api/kcorp/message/Receiver.java
@@ -13,49 +13,51 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 import api.kcorp.model.ContactForm;
 import api.kcorp.model.EmailForm;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Component
 public class Receiver {
 
 	private final JavaMailSender mailSender;
+	private final SpringTemplateEngine templateEngine;
 
 	@Value("${spring.mail.username}")
 	private String senderEmail;
 
 	@Autowired
-	public Receiver(JavaMailSender mailSender) {
+	public Receiver(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
 		this.mailSender = mailSender;
+		this.templateEngine = templateEngine;
 	}
 
 	@JmsListener(destination = "shreebala.submitfeedback", containerFactory = "myFactory")
     public void receiveMessage(ContactForm form) {
-        System.out.println("Received <" + form + ">");
-        
         //Send mail.
         try {
 	        MimeMessage msg = mailSender.createMimeMessage();
 	        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 	        
-	        helper.setFrom(senderEmail);
+	        helper.setFrom("contact@shreebalanrithyalaya.com");
 	        helper.setTo("contact@shreebalanrithyalaya.com");
+	        helper.setReplyTo(form.getEmail());
 	        helper.setSubject("New comment received on shreebalanrithyalaya.com");
 	        helper.setText(	"New comment received with the following details\n\nName: " + form.getName() 
 	        				+ "\nEmail: " + form.getEmail() 
 	        				+ "\nSubject: " + form.getSubject()
 	        				+ "\nMessage: " + form.getMessage() 
 	        				+ "\nSubscription: " + (form.getOptin()? "Yes" : "No") + "\n\nThanks\nShreebala Nrithyalaya\n\nNote: A confirmation mail has been sent to the sender's mail address");
-	        
 	        mailSender.send(msg);
-	        System.out.println("Mail 1 sent");
+
+	        helper.setFrom("contact@shreebalanrithyalaya.com");
 	        helper.setTo(form.getEmail());
 	        helper.setReplyTo("contact@shreebalanrithyalaya.com");
-	        helper.setSubject("Thank you for your valuable comment.");
-	        helper.setText("We respect your feedback.\n"
-	        				+ "As we are constantly striving to provide the best learning experience to our students, we value your feedback and take actions if required."
-	        				+ "Thank You\nShreebala Nrithyalaya", "<html><body><h1>We respect your feedback.</h1><p>As we are constantly striving to provide the best learning experience to our students, we value your feedback and take actions if required.</p><a href='http://www.shreebalanrithyalaya.com'>Shreebala Nrithyalaya</a></body></html>");
-
+	        helper.setSubject("Thank you for your enquiry / suggestion.");
+            Context context = new Context();
+            context.setVariable("name", form.getName());
+            String html = templateEngine.process("email_shreebala", context);
+	        helper.setText(html, true);
 	        mailSender.send(msg);
-        	System.out.println("Mail 2 sent");
         }
         catch(MailException | MessagingException me) {
         	System.err.println(me.getMessage());
@@ -64,12 +66,11 @@ public class Receiver {
     
     @JmsListener(destination = "simplyvijay.emailvijay", containerFactory = "myFactory")
     public void receiveEmailNotification(EmailForm form) {
-    	System.out.println("Received <" + form + ">");
-    	
         //Send mail.
         try {
 	        MimeMessage msg = mailSender.createMimeMessage();
 	        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
 	        helper.setFrom(senderEmail);
 	        helper.setTo("contact@simplyvijay.com");
 	        helper.setSubject("New message received on simplyvijay.com");
@@ -81,8 +82,17 @@ public class Receiver {
 	        				+ "\nJob Description: " + form.getJd()
 	        				+ "\nDivision: " + form.getDivision()
 	        				+ "\n\nThanks\nVijay");
-	        
 	        mailSender.send(msg);
+
+	        helper.setFrom(senderEmail);
+	        helper.setTo(form.getEmail());
+	        helper.setReplyTo("contact@simplyvijay.com");
+	        helper.setSubject("Thank you for your enquiry");
+            Context context = new Context();
+            context.setVariable("name", form.getName());
+            String html = templateEngine.process("email_simplyvijay", context);
+            helper.setText(html, true);
+            mailSender.send(msg);
         }
         catch(MailException | MessagingException me) {
         	System.err.println(me.getMessage());
